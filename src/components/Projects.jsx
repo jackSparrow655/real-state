b0b26db78eeaa9bd6e668879fe9d0d2db113833d
Arijit655 <arijitbarik655@gmail.com>
2025-01-19 10:07:21 +0530

    handle responsiveness in project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,7 +20,7 @@ const Projects = () => {
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis, quo.</p>
 
       {/* ---------------slider button----------- */}
-      <div className='hidden md:flex justify-end items-center mb-8'>
+      <div className='hidden sm:flex justify-end items-center mb-8'>
         <button onClick={() => handleScroll("left")} className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
             <img src={assets.left_arrow} alt="left arrow" />
         </button>
@@ -31,9 +31,9 @@ const Projects = () => {
       {/* --------------project slider container------------- */}
       <div ref={scrollRef} className='overflow-y-auto md:overflow-x-auto'>
         {/* <div className='grid grid-cols-1 md:grid-cols-4 gap-10'> */}
-        <div className='grid grid-flow-row auto-rows-max sm:grid-flow-col sm:auto-cols-max gap-10'>
+        <div className='grid grid-flow-col auto-cols-max gap-5 sm:gap-10'>
             {projectsData.map((project, index) => (
-                <div key={index} className='relative mt-10 h-[400px] w-[280px]'>
+                <div key={index} className='relative mt-10 h-[400px] w-[200px] sm:w-[280px]'>
                     <img src={project.image} alt="project-imagew" className='rounded-md' />
                     <div className='border w-fit px-5 py-2 absolute translate-x-10 -translate-y-1/2 float-start bg-neutral-100 rounded-md'>
                         <h3 className='text-xl font-semibold tracking-wide'>{project.title}</h3>
